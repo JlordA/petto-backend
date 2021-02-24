338acef667c0545e61634eaf912159fb8e59614a
JlordA <james.ardery@gmail.com>
2021-02-24 16:12:05 -0500

    Serializer folder deleted for Deployment

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::UsersController < ApplicationController
-
+    
     def index
-        users = User.all
+        # users = User.all
+        users = User.all.includes(:pets)
         render json: users
     end
     
@@ -36,7 +37,6 @@ end
 #     user = User.find_by(:name => params[:name]) 
 #     render json: user
 # end
-# users = User.all.includes(:pets)
 
  #    else
         #     user = User.find(params[:id])
--- app/serializers/user_serializer.rb
+++
@@ -1,6 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id
-  attributes :name
-
-  has_many :pets
-end
